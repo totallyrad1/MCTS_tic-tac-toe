705820ba25c4a0bad4e8007d533407dc7bb44146
Asaad Naji <assaadnaji014@gmail.com>
2025-01-09 22:51:32 +0100

    ucb calculation

--- src/Node.cpp
+++ src/Node.cpp
@@ -2,6 +2,6 @@
 
 float Node::UCB1(){
     int value = state.currentPlayer;
-    float p1 = (value ? 0 : -1) + ((((score * 1.0f) / visited) + 1) / 2.0f);
-    return p1 + (1.41f * (sqrt(visited) / (visited * 1.0f)));
+    float p1 = (value ? 0 : - 1) + ((((score * 1.0f) / visited) + 1) / 2.0f);
+    return p1 + (1.41f * (sqrt(Parent->visited) / (visited * 1.0f)));
 }
